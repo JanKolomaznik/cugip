de38d13d278cbc34426763ed2b16e206a0b63b7d
Jan Kolomaznik <honza.kolomaznik@gmail.com>
2014-10-17 15:44:58 +0200

    Shortest distance watershed transformation.

--- include/cugip/basic_filters/watershed_transformation.hpp
+++ include/cugip/basic_filters/watershed_transformation.hpp
@@ -150,32 +150,139 @@ step4(TImageView aImageView, TIdImageView aIdImageView)
 
 template <typename TImageView, typename TIdImageView>
 void
-watershed_transformation(TImageView aImageView, TIdImageView aIdImageView)
+watershed_transformation(TImageView aImageView, TIdImageView aIdImageView, TIdImageView aTmpIdImageView)
 {
-
 	step1(aImageView, aIdImageView);
-	step2(aImageView, aIdImageView);
+	step2(aImageView, aIdImageView, aTmpIdImageView);
 	step3(aImageView, aIdImageView);
 	step4(aImageView, aIdImageView);
-/*
-	device_flag lutUpdatedFlag;
-	lutUpdatedFlag.reset_host();
-
-	D_PRINT("Watershed transform initialization ...");
-	detail::init_lut(aImageView, aLUT);
-	detail::scan_image(aImageView, aLUT, lutUpdatedFlag.view());
-
-	int i = 0;
-	while (lutUpdatedFlag.check_host()) {
-		D_PRINT("    Running WShed iteration ..." << ++i);
-		lutUpdatedFlag.reset_host();
-
-		detail::update_lut(aImageView, aLUT);
-		detail::update_labels(aImageView, aLUT);
-		detail::scan_image(aImageView, aLUT, lutUpdatedFlag.view());
-	}
-	D_PRINT("Watershed transform done!");*/
 }
+//**************************************************
+template<typename TLabel, typename TDistance>
+struct init_wsheds_ftor
+{
+	init_wsheds_ftor(TDistance aInfinity)
+		: infinity(aInfinity)
+	{}
+
+	CUGIP_DECL_HYBRID void
+	operator()(const TLabel &aLabel, TDistance &aDistance)
+	{
+		aDistance = aLabel == 0 ? infinity : 0;
+	}
+
+	TDistance infinity;
+};
+
+template <typename TIdImageView, typename TDistanceView>
+void
+init_watershed_buffers(TIdImageView aIdImageView, TDistanceView aDistanceView)
+{
+	for_each(
+		aIdImageView,
+		aDistanceView,
+		init_wsheds_ftor<typename TIdImageView::value_type, typename TDistanceView::value_type>(65000)); //TODO
+}
+//**************************************************
+template <typename TImageView, typename TIdImageView, typename TDistanceView>
+CUGIP_GLOBAL void
+watershed_evolution_kernel(
+		TImageView aImageView,
+		TIdImageView aIdImageView,
+		TIdImageView aTmpIdImageView,
+		TDistanceView aDistanceView1,
+		TDistanceView aDistanceView2,
+		device_flag_view aUpdateFlag)
+{
+	typedef TImageView::value_type Value;
+	typedef TIdImageView::value_type Label;
+	typedef TIdImageView::locator LabelLocator;
+	typedef TDistanceView::locator Locator;
+	typedef typename TDistanceView::value_type Distance;
+
+	typename TView::coord_t coord(blockIdx.x * blockDim.x + threadIdx.x, blockIdx.y * blockDim.y + threadIdx.y);
+	typename TView::extents_t extents = aView.dimensions();
+
+	if (coord < extents) {
+		Value value = aImageView[coord];
+		Locator distanceLocator = aDistanceView1.template locator<cugip::border_handling_repeat_t>(coord);
+		Distance currentDistance = distanceLocator.get();
+		LabelLocator labelLocator = aIdImageView.template locator<cugip::border_handling_repeat_t>(coord);
+		Label label = labelLocator.get();
+
+		Distance currentMinimum = max(0, currentDistance - value);
+		typename Locator::diff_t offset;
+		bool found = false;
+		for (int j = -1; j <= 1; ++j) {
+			for (int i = -1; i <= 1; ++i) {
+				Distance distance = aLocator[typename Locator::diff_t(i, j)];
+				if (distance < currentMinimum) {
+					currentMinimum = distance;
+					offset = typename Locator::diff_t(i, j);
+					found = true;
+				}
+			}
+		}
+		if (found) {
+			aUpdateFlag.set_device();
+			currentDistance = currentMinimum + value;
+			label = labelLocator[offset];
+		}
+		aDistanceView2[coord] = currentDistance;
+		aTmpIdImageView[coord] = label;
+	}
+
+}
+
+template <typename TImageView, typename TIdImageView, typename TDistanceView>
+void
+watershed_evolution(
+		TImageView aImageView,
+		TIdImageView aIdImageView,
+		TIdImageView aTmpIdImageView,
+		TDistanceView aDistanceView1,
+		TDistanceView aDistanceView2,
+		device_flag_view aUpdateFlag)
+{
+	dim3 blockSize(256, 1, 1);
+	dim3 gridSize((aImageView.dimensions().template get<0>() / blockSize.x + 1), aImageView.dimensions().template get<1>() / blockSize.y + 1, 1);
+
+	watershed_evolution_kernel<<< gridSize1D, blockSize1D >>>(
+				aImageView,
+				aIdImageView,
+				aTmpIdImageView,
+				aDistanceView1,
+				aDistanceView2,
+				aUpdateFlag);
+}
+
+
+template <typename TImageView, typename TIdImageView, typename TDistanceView>
+void
+watershed_transformation2(
+		TImageView aImageView,
+		TIdImageView aIdImageView,
+		TIdImageView aTmpIdImageView,
+		TDistanceView aDistanceView1,
+		TDistanceView aDistanceView2
+		)
+{
+	device_flag updatedFlag;
+	updatedFlag.reset_host();
+
+
+	init_watershed_buffers(labeledRegions, tmp);
+	while (updatedFlag.check_host()) {
+		watershed_evolution(
+				aImageView,
+				aIdImageView,
+				aTmpIdImageView,
+				aDistanceView1,
+				aDistanceView2,
+				updatedFlag.view());
+	}
+}
+
 
 
 }//namespace cugip
