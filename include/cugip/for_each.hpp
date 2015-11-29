bee07909f9efc96f01db576414ce3ea8f4535cb0
Jan Kolomaznik <honza.kolomaznik@gmail.com>
2015-11-30 00:50:08 +0100

    Fixing for_each block and grid sizes.

--- include/cugip/for_each.hpp
+++ include/cugip/for_each.hpp
@@ -3,16 +3,50 @@
 #include <cugip/detail/include.hpp>
 #include <cugip/meta_algorithm.hpp>
 #include <cugip/exception.hpp>
+#include <cugip/cuda_utils.hpp>
 
 namespace cugip {
 
 namespace detail {
 
+template<int tDimension>
+dim3 defaultBlockDimForDimension();
+
+template<>
+inline dim3 defaultBlockDimForDimension<2>() { return dim3(32, 16, 1); }
+
+template<>
+inline dim3 defaultBlockDimForDimension<3>() { return dim3(32, 4, 4); }
+
+template<int tDimension>
+dim3 defaultGridSizeForBlockDim(cugip::simple_vector<int, tDimension> aViewDimensions, dim3 aBlockSize);
+
+template<>
+inline dim3
+defaultGridSizeForBlockDim<2>(cugip::simple_vector<int, 2> aViewDimensions, dim3 aBlockSize)
+{
+	return dim3(
+		(aViewDimensions[0] - 1) / aBlockSize.x + 1,
+		(aViewDimensions[1] - 1) / aBlockSize.y + 1,
+		1);
+}
+
+template<>
+inline dim3
+defaultGridSizeForBlockDim<3>(cugip::simple_vector<int, 3> aViewDimensions, dim3 aBlockSize)
+{
+	return dim3(
+		(aViewDimensions[0] - 1) / aBlockSize.x + 1,
+		(aViewDimensions[1] - 1) / aBlockSize.y + 1,
+		(aViewDimensions[2] - 1) / aBlockSize.z + 1);
+}
+
+
 template <typename TView, typename TFunctor>
 CUGIP_GLOBAL void
 kernel_for_each(TView aView, TFunctor aOperator )
 {
-	typename TView::coord_t coord(blockIdx.x * blockDim.x + threadIdx.x, blockIdx.y * blockDim.y + threadIdx.y);
+	typename TView::coord_t coord = mapBlockIdxAndThreadIdxToViewCoordinates<dimension<TView>::value>();
 	typename TView::extents_t extents = aView.dimensions();
 
 	if (coord < extents) {
@@ -32,8 +66,8 @@ template <typename TView, typename TFunctor>
 void
 for_each(TView aView, TFunctor aOperator)
 {
-	dim3 blockSize(256, 1, 1);
-	dim3 gridSize((aView.dimensions().template get<0>() / blockSize.x + 1), aView.dimensions().template get<1>() / blockSize.y + 1, 1);
+	dim3 blockSize = detail::defaultBlockDimForDimension<dimension<TView>::value>();
+	dim3 gridSize = detail::defaultGridSizeForBlockDim(aView.dimensions(), blockSize);
 
 	D_PRINT("Executing kernel: blockSize = "
 	               << blockSize
@@ -57,7 +91,7 @@ template <typename TView1, typename TView2, typename TFunctor>
 CUGIP_GLOBAL void
 kernel_for_each(TView1 aView1, TView2 aView2, TFunctor aOperator )
 {
-	typename TView1::coord_t coord(blockIdx.x * blockDim.x + threadIdx.x, blockIdx.y * blockDim.y + threadIdx.y);
+	typename TView1::coord_t coord = mapBlockIdxAndThreadIdxToViewCoordinates<dimension<TView1>::value>();
 	typename TView1::extents_t extents = aView1.dimensions();
 
 	if (coord < extents) {
@@ -79,8 +113,8 @@ for_each(TView1 aView1, TView2 aView2, TFunctor aOperator)
 {
 	CUGIP_ASSERT(aView1.dimensions() == aView2.dimensions());
 
-	dim3 blockSize(256, 1, 1);
-	dim3 gridSize((aView1.dimensions().template get<0>() / blockSize.x + 1), aView1.dimensions().template get<1>() / blockSize.y + 1, 1);
+	dim3 blockSize = detail::defaultBlockDimForDimension<dimension<TView1>::value>();
+	dim3 gridSize = detail::defaultGridSizeForBlockDim(aView1.dimensions(), blockSize);
 
 	D_PRINT("Executing kernel: blockSize = "
 	               << blockSize
@@ -104,7 +138,7 @@ template <typename TView, typename TFunctor>
 CUGIP_GLOBAL void
 kernel_for_each_position(TView aView, TFunctor aOperator )
 {
-	typename TView::coord_t coord(blockIdx.x * blockDim.x + threadIdx.x, blockIdx.y * blockDim.y + threadIdx.y);
+	typename TView::coord_t coord = mapBlockIdxAndThreadIdxToViewCoordinates<dimension<TView>::value>();
 	typename TView::extents_t extents = aView.dimensions();
 
 	if (coord < extents) {
@@ -121,8 +155,8 @@ template <typename TView, typename TFunctor>
 void
 for_each_position(TView aView, TFunctor aOperator)
 {
-	dim3 blockSize(256, 1, 1);
-	dim3 gridSize((aView.dimensions().template get<0>() / blockSize.x + 1), aView.dimensions().template get<1>() / blockSize.y + 1, 1);
+	dim3 blockSize = detail::defaultBlockDimForDimension<dimension<TView>::value>();
+	dim3 gridSize = detail::defaultGridSizeForBlockDim(aView.dimensions(), blockSize);
 
 	D_PRINT("Executing kernel: for_each_position(); blockSize = "
 	               << blockSize
@@ -146,7 +180,7 @@ template <typename TView, typename TFunctor>
 CUGIP_GLOBAL void
 kernel_for_each_locator(TView aView, TFunctor aOperator)
 {
-	typename TView::coord_t coord(blockIdx.x * blockDim.x + threadIdx.x, blockIdx.y * blockDim.y + threadIdx.y);
+	typename TView::coord_t coord = mapBlockIdxAndThreadIdxToViewCoordinates<dimension<TView>::value>();
 	typename TView::extents_t extents = aView.dimensions();
 
 	if (coord < extents) {
@@ -163,8 +197,8 @@ template <typename TView, typename TFunctor>
 void
 for_each_locator(TView aView, TFunctor aOperator)
 {
-	dim3 blockSize(256, 1, 1);
-	dim3 gridSize((aView.dimensions().template get<0>() / blockSize.x + 1), aView.dimensions().template get<1>() / blockSize.y + 1, 1);
+	dim3 blockSize = detail::defaultBlockDimForDimension<dimension<TView>::value>();
+	dim3 gridSize = detail::defaultGridSizeForBlockDim(aView.dimensions(), blockSize);
 
 	D_PRINT("Executing kernel: for_each_locator(); blockSize = "
 	               << blockSize
@@ -189,7 +223,7 @@ template <typename TView1, typename TView2, typename TFunctor>
 CUGIP_GLOBAL void
 kernel_for_each_locator(TView1 aView1, TView2 aView2, TFunctor aOperator)
 {
-	typename TView1::coord_t coord(blockIdx.x * blockDim.x + threadIdx.x, blockIdx.y * blockDim.y + threadIdx.y);
+	typename TView1::coord_t coord = mapBlockIdxAndThreadIdxToViewCoordinates<dimension<TView1>::value>();
 	typename TView1::extents_t extents = aView1.dimensions();
 
 	if (coord < extents) {
@@ -211,8 +245,8 @@ for_each_locator(TView1 aView1, TView2 aView2, TFunctor aOperator)
 {
 	CUGIP_ASSERT(aView1.dimensions() == aView2.dimensions());
 
-	dim3 blockSize(256, 1, 1);
-	dim3 gridSize((aView1.dimensions().template get<0>() / blockSize.x + 1), aView1.dimensions().template get<1>() / blockSize.y + 1, 1);
+	dim3 blockSize = detail::defaultBlockDimForDimension<dimension<TView1>::value>();
+	dim3 gridSize = detail::defaultGridSizeForBlockDim(aView1.dimensions(), blockSize);
 
 	D_PRINT("Executing kernel: for_each_locator(); blockSize = "
 	               << blockSize
