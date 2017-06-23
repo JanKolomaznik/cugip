9d653bfe1f0cd55fccb7854f040a8e22eeb32755
Jan Kolomaznik <honza.kolomaznik@gmail.com>
2017-06-24 00:29:17 +0200

    Preparation of multiresolution pyramid computation.

---
+++ include/cugip/multiresolution_pyramid.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace cugip {
+
+template<typename TImage>
+class multiresolution_pyramid {
+
+};
+
+template<typename TImageView>
+class multiresolution_pyramid_view {
+
+};
+
+template<typename TInputView, typename TPyramidView, typename TAlgorithm>
+void compute_multiresolution_pyramid()
+
+
+}  //namespace cugip
